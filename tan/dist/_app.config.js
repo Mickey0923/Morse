5b318c0ce77c6d199b5fafa9bf92e7788a40158e
Mickey0923 <474792066@qq.com>
2024-09-14 22:07:26 +0800

    1

--- tan/dist/_app.config.js
+++
@@ -1,1 +0,0 @@
-window.__PRODUCTION__SMART_COMMUNITY__CONF__={"VITE_GLOB_APP_TITLE":"Community","VITE_GLOB_APP_SHORT_NAME":"smart_community","VITE_GLOB_API_URL":"https://apigw.ankechain.cn/bkbig","VITE_GLOB_API_URL_PREFIX":""};Object.freeze(window.__PRODUCTION__SMART_COMMUNITY__CONF__);Object.defineProperty(window,"__PRODUCTION__SMART_COMMUNITY__CONF__",{configurable:false,writable:false,});
